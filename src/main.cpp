45573673168b2a21c79ba069deeb3954aefdcdea
Ryan <ryanjgolden00@gmail.com>
2026-06-20 18:06:40 -0400

    Added gravity and jumping, platform collision, game over screen and reset functionality

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,18 @@ void drawLight(Vector2 position, float radius, Color color)
 
 void drawSideArt(int screenW, int screenH)
 {
-    for (int y = 0; y < screenH; y += 40)
+    static Texture2D sideArt = LoadTexture("src/resources/sideArt.png");
+    DrawTexturePro( sideArt, 
+                    {0.0f, 0.0f, static_cast<float>(screenW), static_cast<float>(screenH)}, 
+                    {0.0f, 0.0f, static_cast<float>(sideArt.width), static_cast<float>(sideArt.width)}, 
+                    {0.0f, 0.0f}, 
+                    0.0f, 
+                    WHITE);
+    /*for (int y = 0; y < screenH; y += 40)s
     {
-        DrawRectangle(0, y, screenW, 20, Color{45, 45, 70, 255});
-    }
+        DrawRectangle(0, y, screenW, 20, RED);
+        DrawRectangle(0, y+20, screenW, 20, WHITE);
+    }*/
 }
 
 void toggleFullscreenWindow(){
@@ -32,6 +40,18 @@ void toggleFullscreenWindow(){
         ToggleFullscreen();
         SetWindowSize(Global::startScreenWidth, Global::startScreenHeight);
     } 
+}
+
+void resetPlatforms(auto& platforms){
+    for (size_t i{0}; i < platforms.size(); i++) {
+        platforms[i].rect = {
+            static_cast<float>(GetRandomValue(10, 170)), 
+            400.0f - (static_cast<float>(i)*30.0f*3) - 30.0f,               
+            static_cast<float>(GetRandomValue(20, 70)),                          
+            10.0f                              
+        };
+        platforms[i].colorIndex = 0;
+    }
 }
 
 struct Platform {
@@ -49,6 +69,7 @@ int main(){
     float scale{1.0f};
     float offsetX{0.0f};
     float offsetY{0.0f};
+    Color screenTint = LIGHTGRAY;
 
     // backgrounds
     std::array<Texture2D, 5> backgrounds = {
@@ -60,17 +81,25 @@ int main(){
     }; 
     Texture2D* currentBackground = &backgrounds[0];
     Texture2D* nextBackground = &backgrounds[1];
+
     float bgY{0.0f};
     float bgY2{-480.0f};
     float scoreHeight{0};
     float scrollSpeed{120.0f};
     float scoreSpeed{2.0f}; // 2 per second
     bool changeBackground{false};
+    bool isScrolling{false};
     int backgroundIndex{2}; // init 2 for next background
     int nextLevel{20};
 
     // player
     Rectangle player{Global::screenWidth / 2.0f - 16.0f, 360.0f, 32.0f, 32.0f};
+    Texture2D playerTexture = LoadTexture("src/resources/player.png"); 
+    bool inAir{true};
+    float jumpVelocity{0.0f};
+    float gravity{1500.0f};
+    bool onPlatform{false};
+    bool gameOver{false};
 
     // camera
     GameCamera gameCamera;
@@ -81,19 +110,18 @@ int main(){
     // platforms
     int currentColorIndex{0};
     int nextColorIndex{1};
-    float spacing{60.0f};
+    float spacing{30.0f};
     std::array<Color, 5> platformColors = {GRAY, DARKBLUE, YELLOW, PINK, BROWN};
     std::array<Platform, 10> platforms;
     for (size_t i{0}; i < platforms.size(); i++) {
         platforms[i].rect = {
-            static_cast<float>(GetRandomValue(20, 160)), 
+            static_cast<float>(GetRandomValue(10, 170)), 
             400.0f - (static_cast<float>(i)*spacing*3) - spacing,               
-            static_cast<float>(GetRandomValue(50, 100)),                          
-            20.0f                              
+            static_cast<float>(GetRandomValue(20, 70)),                          
+            10.0f                              
         };
         platforms[i].colorIndex = 0;
     }
-
     float nextPlatformY = platforms.back().rect.y - spacing;
 
     // light map
@@ -102,87 +130,142 @@ int main(){
     RenderTexture2D target = LoadRenderTexture(Global::screenWidth, Global::screenHeight);
     SetTextureFilter(target.texture, TEXTURE_FILTER_POINT); 
 
+    // ------------------------ //
     while (!WindowShouldClose()){
 
         // first pass - game logic
         if(IsKeyPressed(KEY_F11)) toggleFullscreenWindow();
 
+        if(IsKeyPressed(KEY_R) && gameOver) {
+            resetPlatforms(platforms);
+            scoreHeight = 0;
+            isScrolling = false;
+            gameOver = false;
+            screenTint = LIGHTGRAY;
+            currentBackground = &backgrounds[0];
+            nextBackground = &backgrounds[1];
+            currentColorIndex = 0;
+            nextColorIndex = 1;
+            bgY = 0.0f;
+            bgY2 = -480.0f;
+            nextLevel = 20;
+        }
+
         // mouse for world
-        Vector2 mouse = GetMousePosition();
+        /*Vector2 mouse = GetMousePosition();
         Vector2 virtualMouse = {
             (mouse.x - offsetX) / scale,
             (mouse.y - offsetY) / scale
-        };
+        };*/
 
-        // player
-        float dt = GetFrameTime();
-        if (IsKeyDown(KEY_LEFT)) player.x -= Global::playerSpeed * dt;
-        if (IsKeyDown(KEY_RIGHT)) player.x += Global::playerSpeed * dt;
-        if (IsKeyDown(KEY_UP)) player.y -= Global::playerSpeed * dt;
-        if (IsKeyDown(KEY_DOWN)) player.y += Global::playerSpeed * dt;
+        if(!gameOver){
+            // player
+            float dt = GetFrameTime();
+            if (IsKeyDown(KEY_A)) player.x -= Global::playerSpeed * dt;
+            if (IsKeyDown(KEY_D)) player.x += Global::playerSpeed * dt;
 
-        if(player.x > Global::screenWidth + 32.0f) player.x = -64.0f;
-        if(player.x < -64.0f) player.x = Global::screenWidth + 32.0f;
+            if(player.x > Global::screenWidth + 32.0f) player.x = -64.0f;
+            if(player.x < -64.0f) player.x = Global::screenWidth + 32.0f;
 
-        if(player.y < 96) {
-            scrollSpeed = 240.0f; 
-            scoreSpeed = 4.0f;
-        }
-        else {
-            scrollSpeed = 120.0f; 
-            scoreSpeed = 2.0f;
-        }
-
-        // current background
-        bgY += scrollSpeed * dt;
-        if (bgY >= Global::screenHeight) bgY -= Global::screenHeight;
-
-        // incoming background
-        if(changeBackground){
-            bgY2 += scrollSpeed *dt;
-            if (bgY2 >= 0.0f) {
-                changeBackground = false;
-                currentBackground = nextBackground;
-                nextBackground = &backgrounds[backgroundIndex];
-
-                currentColorIndex = nextColorIndex;
-                nextColorIndex = backgroundIndex;
-
-                backgroundIndex++;
-                if(backgroundIndex > 4) backgroundIndex = 0; // loop through backgrounds (temp for now)
-                bgY2 = -static_cast<float>(Global::screenHeight); // reset position for next level change
+            if(player.y < 96) {
+                scrollSpeed = 360.0f; 
+                scoreSpeed = 6.0f;
             }
-        }
+            else if(player.y < 240) {
+                scrollSpeed = 240.0f; 
+                scoreSpeed = 4.0f;
+            }
+            else {
+                scrollSpeed = 120.0f; 
+                scoreSpeed = 2.0f;
+            }
+            
+            if(IsKeyPressed(KEY_SPACE) && !inAir){
+                inAir = true;
+                jumpVelocity -= 600.0f;
+                isScrolling = true;
+            }
 
-        // change level based on height/score
-        scoreHeight += scoreSpeed * dt;
-        if(!changeBackground && static_cast<int>(scoreHeight) >= nextLevel) {
-            changeBackground = true;
-            nextLevel += 20;
-        }
+            float previousPlayerY = player.y;
+            if(inAir || onPlatform) jumpVelocity += gravity * dt;
+            player.y += jumpVelocity * dt;
 
-        // platforms 
-        for (Platform& platform : platforms) {
-            platform.rect.y += scrollSpeed * dt;
-                        
-            // if platform is below the camera view
-            if (platform.rect.y > Global::screenHeight) {
-                platform.rect.x = static_cast<float>(GetRandomValue(20, 160));
-                platform.rect.y = nextPlatformY;
-                platform.rect.width = static_cast<float>(GetRandomValue(50, 100));     
-            }    
+            if(!isScrolling){
+                if(player.y >= Global::screenHeight - 32.0f){
+                    player.y = Global::screenHeight - 32.0f;
+                    inAir = false;
+                    jumpVelocity = 0.0f;
+                }
+            }
+        
+            if(isScrolling){
+                // current background
+                bgY += scrollSpeed * dt;
+                if (bgY >= Global::screenHeight) bgY -= Global::screenHeight;
 
-            if (changeBackground) {
-                float transitionY = bgY2 + static_cast<float>(Global::screenHeight);
-                float platformEdgeY = platform.rect.y + platform.rect.height;
-                if (platformEdgeY < transitionY) platform.colorIndex = nextColorIndex;
-                else platform.colorIndex = currentColorIndex;
+                // incoming background
+                if(changeBackground){
+                    bgY2 += scrollSpeed *dt;
+                    if (bgY2 >= 0.0f) {
+                        changeBackground = false;
+                        currentBackground = nextBackground;
+                        nextBackground = &backgrounds[backgroundIndex];
+
+                        currentColorIndex = nextColorIndex;
+                        nextColorIndex = backgroundIndex;
+
+                        backgroundIndex++;
+                        if(backgroundIndex > 4) backgroundIndex = 0; // loop through backgrounds (temp for now)
+                        bgY2 = -static_cast<float>(Global::screenHeight); // reset position for next level change
+                    }
+                }
+
+                // change level based on height/score
+                scoreHeight += scoreSpeed * dt;
+                if(!changeBackground && static_cast<int>(scoreHeight) >= nextLevel) {
+                    changeBackground = true;
+                    nextLevel += 20;
+                }
+
+                if(player.y >= Global::screenHeight + 128.0f){
+                    gameOver = true;
+                    inAir = false;
+                    jumpVelocity = 0.0f;
+                }
+
+                // platforms 
+                for (Platform& platform : platforms) {
+                    platform.rect.y += scrollSpeed * dt;
+                                
+                    // if platform is below the camera view
+                    if (platform.rect.y > Global::screenHeight) {
+                        platform.rect.x = static_cast<float>(GetRandomValue(10, 170));
+                        platform.rect.y = nextPlatformY;
+                        platform.rect.width =  static_cast<float>(GetRandomValue(20, 70)); 
+                    }    
+
+                    if (changeBackground) {
+                        float transitionY = bgY2 + static_cast<float>(Global::screenHeight);
+                        float platformEdgeY = platform.rect.y + platform.rect.height;
+                        if (platformEdgeY < transitionY) platform.colorIndex = nextColorIndex;
+                        else platform.colorIndex = currentColorIndex;
+                    } 
+
+                    float platformTop = platform.rect.y;
+                    if (jumpVelocity > 0.0f && previousPlayerY + player.height <= platformTop && player.y + player.height >= platformTop && CheckCollisionRecs(player, platform.rect)){
+                            player.y = platform.rect.y - player.height;
+                            jumpVelocity = 0.0f;
+                            inAir = false;
+                            onPlatform = true;
+                            jumpVelocity -= static_cast<float>(GetRandomValue(500, 700));
+                    }
+                }
             }
         }
 
         // second pass - lightMap
         BeginTextureMode(lightMap);
-            ClearBackground(LIGHTGRAY); // screen tint (black for complete dark, etc.)
+            ClearBackground(screenTint); // screen tint (black for complete dark, etc.)
             BeginBlendMode(BLEND_ADDITIVE); 
                 //drawLight(virtualMouse, 150.0f, Color{255, 240, 200, 255});
                 drawLight({player.x + 16.0f, player.y + 16.0f}, 75.0f, Color{255, 191, 0, 255});
@@ -206,12 +289,19 @@ int main(){
     
                 for (Platform& platform : platforms) {
                     DrawRectangleRec(platform.rect, platformColors[platform.colorIndex]);
+                    if(CheckCollisionRecs(platform.rect, player)) {
+                        DrawRectangleLinesEx(platform.rect, 1.0f, RED);  
+                    }  
                 }
                 
-                DrawRectangle(player.x, player.y, player.width, player.height, RED);
+                DrawTexture(playerTexture, player.x, player.y, WHITE);
 
                 DrawText(TextFormat("Height: %i", static_cast<int>(scoreHeight)), 20, 20, 20, YELLOW);
                 DrawText(TextFormat("FPS: %i", GetFPS()), 160, 20, 20, RED);
+                if(gameOver) {
+                    screenTint = RED;
+                    DrawText("GAME OVER!", 70.0f, 240.0f, 20, BLACK); 
+                }
 
             EndMode2D();
 
